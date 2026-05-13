d76b11fa0e489d0371496fd4b8c924d35961e556
rkdfmd <adh52@naver.com>
2026-05-13 11:45:33 +0900

    Change spinner message from '오픈파일럿' to '강릉파일럿'

--- system/manager/build.py
+++ system/manager/build.py
@@ -55,7 +55,7 @@ def build(spinner: Spinner, dirty: bool = False, minimal: bool = False) -> None:
         elif len(line):
           compile_output.append(line)
           line_text = line.decode('utf8', 'replace')
-          spinner.update("오픈파일럿 준비중")
+          spinner.update("강릉파일럿 준비중")
           print(line_text)
       except Exception:
         pass
